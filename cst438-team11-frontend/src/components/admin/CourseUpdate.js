57dc0b90704d7a22e06da583975da504697d6a26
Gomez-V <160459958+Gomez-V@users.noreply.github.com>
2025-04-15 16:55:34 -0700

    Update CourseUpdate.js

--- cst438-team11-frontend/src/components/admin/CourseUpdate.js
+++ cst438-team11-frontend/src/components/admin/CourseUpdate.js
@@ -5,6 +5,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import {SERVER_URL} from '../../Constants';
 
 const CourseUpdate = (props)  => {
 
